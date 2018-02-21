6ef126692374cf075419e466323b7d49b0339a77
Ian Santopietro <ian@system76.com>
2018-02-21 16:36:12 -0700

    Add pyflakes to build process

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
- Copyright 2017 Ian Santopietro <isantop@gmail.com>
+ Copyright 2017-2018 Ian Santopietro <isantop@gmail.com>
 
 Permission to use, copy, modify, and/or distribute this software for any purpose
 with or without fee is hereby granted, provided that the above copyright notice
@@ -14,10 +14,63 @@ INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
 OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
 TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
+
+Portions of test-related code authored by Jason DeRose <jason@system76.com>
 """
 
 
 from distutils.core import setup
+from distutils.cmd import Command
+import os, subprocess, sys
+
+TREE = os.path.dirname(os.path.abspath(__file__))
+DIRS = [
+    'kernelstub',
+    'bin']
+
+
+def run_under_same_interpreter(opname, script, args):
+    print('\n** running: {}...'.format(script), file=sys.stderr)
+    if not os.access(script, os.R_OK | os.X_OK):
+        print('ERROR: cannot read and execute: {!r}'.format(script),
+            file=sys.stderr
+        )
+        print('Consider running `setup.py test --skip-{}`'.format(opname),
+            file=sys.stderr
+        )
+        sys.exit(3)
+    cmd = [sys.executable, script] + args
+    print('check_call:', cmd, file=sys.stderr)
+    subprocess.check_call(cmd)
+    print('** PASSED: {}\n'.format(script), file=sys.stderr)
+
+def run_pyflakes3():
+    script = '/usr/bin/pyflakes3'
+    names = [
+        'setup.py',
+    ] + DIRS
+    args = [os.path.join(TREE, name) for name in names]
+    run_under_same_interpreter('flakes', script, args)
+
+
+
+class Test(Command):
+    description = 'run pyflakes3'
+
+    user_options = [
+        ('skip-flakes', None, 'do not run pyflakes static checks'),
+    ]
+
+    def initialize_options(self):
+        self.skip_sphinx = 0
+        self.skip_flakes = 0
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if not self.skip_flakes:
+            run_pyflakes3()
 
 setup(name='kernelstub',
     version='2.0.8',
@@ -28,6 +81,7 @@ setup(name='kernelstub',
     license='ISC',
     packages=['kernelstub'],
     scripts=['bin/kernelstub'],
+    cmdclass={'test': Test},
     data_files=[
         ('/etc/kernel/postinst.d', ['data/kernel/zz-kernelstub']),
         ('/etc/initramfs/post-update.d', ['data/initramfs/zz-kernelstub']),
