54913e499de698d4de5a5b573929017721c591c2
Ian Santopietro <ian@system76.com>
2018-09-25 11:07:29 -0600

    Release 3.1.0
    Add systemd journald logging, if the python3 systemd support is installed
    Silently revert to just file logging if so

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class Test(Command):
             run_pyflakes3()
 
 setup(name='kernelstub',
-    version='3.0.0',
+    version='3.1.0',
     description='Automatic kernel efistub manager for UEFI',
     url='https://launchpad.net/kernelstub',
     author='Ian Santopietro',
