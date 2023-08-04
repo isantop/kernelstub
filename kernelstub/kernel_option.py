240dd440664e216ea24bfd9ebbafd36523f6886a
Ian Santopietro <isantop@gmail.com>
2023-08-04 12:24:35 -0600

    fix(kernel_option): change output to debug mode

--- kernelstub/kernel_option.py
+++ kernelstub/kernel_option.py
@@ -23,16 +23,16 @@ import os.path
 def options(path):
     items={}
     for name in os.listdir(path):
-        log.info('Checking item %s', name)
+        log.debug('Checking item %s', name)
         key = None
         if name.startswith("vmlinuz-"):
-            log.info('Item %s is kernel-image', name)
+            log.debug('Item %s is kernel-image', name)
             key = "kernel"
         elif name.startswith("initrd.img-"):
-            log.info('Item %s is debian-style initrd', name)
+            log.debug('Item %s is debian-style initrd', name)
             key = "initrd"
         elif name.startswith("initramfs"):
-            log.info('Item %s is fedora-style initrd', name)
+            log.debug('Item %s is fedora-style initrd', name)
             key = "initrd"
 
         if key is None:
@@ -42,39 +42,39 @@ def options(path):
         version = parts[1]
         if version.endswith('.img'):
             version = version[:-4]
-        log.info('Item version is %s', version)
+        log.debug('Item version is %s', version)
 
         if not version in items:
-            log.info('Adding item %s to list', name)
+            log.debug('Adding item %s to list', name)
             items[version] = {}
 
         items[version][key] = os.path.join(path, name)
 
-    log.info('Found items: %s', items)
+    log.debug('Found items: %s', items)
     return items
 
 def get_newest_option(opts):
-    log.info('Getting latest boot item version')
+    log.debug('Getting latest boot item version')
     latest_version = None
     latest_option = None
 
     for version, option in opts.items():
-        log.info('Checking option %s', option)
+        log.debug('Checking option %s', option)
         # If option is not complete, skip
         if 'kernel' not in option or 'initrd' not in option:
-            log.info('%s does not contain all items, skipping...', version)
+            log.debug('%s does not contain all items, skipping...', version)
             continue
 
         # If this option is newer, store this option and continue
         if latest_version is None or vCompare (version, latest_version):
-            log.info('%s is the latest version', version)
+            log.debug('%s is the latest version', version)
             latest_version = version
             latest_option = option
     
     return latest_option, latest_version
 
 def latest_option(path):
-    log.info('Checking for boot items in %s', path)
+    log.debug('Checking for boot items in %s', path)
     opts = options(path)
     latest_option, latest_version = get_newest_option(opts)
     
@@ -84,8 +84,8 @@ def latest_option(path):
     if len(opts) > 0:
         previous_option, latest_version = get_newest_option(opts)
 
-    log.info('Latest option: %s', latest_option)
-    log.info('Previous option: %s', previous_option)
+    log.debug('Latest option: %s', latest_option)
+    log.debug('Previous option: %s', previous_option)
     return latest_option, previous_option
 
 if __name__ == "__main__":
