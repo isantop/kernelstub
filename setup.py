44e30a06bcb5f9d044b55ad074dda96d9e1bd188
Ian Santopietro <isantop@gmail.com>
2023-08-09 17:19:14 -0600

    chore: update version

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class Test(Command):
             run_pyflakes3()
 
 setup(name='kernelstub',
-    version='4.0.0',
+    version='4.0.1',
     description='Automatic kernel efistub manager for UEFI',
     url='https://launchpad.net/kernelstub',
     author='Ian Santopietro',
